218298c82972d82e4c32f6df0c539d850841450d
Shivam Gupta <shivam39@outlook.com>
2019-06-06 15:10:00 -0700

    Create solution.py

---
+++ Python/Find the Runner-Up Score!/solution.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    high = -101
+    shigh = -101
+    for num in arr:
+        if (num > shigh):
+            if (num < high):
+                shigh = num
+            elif (num > high):
+                shigh = high
+                high = num
+    
+    print(shigh)
