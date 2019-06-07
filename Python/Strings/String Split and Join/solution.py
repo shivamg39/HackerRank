0599d630116ca7ed2b7af59da4be86468d8f9994
Shivam Gupta <shivam39@outlook.com>
2019-06-07 12:22:56 -0700

    Update solution.py

---
+++ Python/Strings/String Split and Join/solution.py
@@ -0,0 +1,8 @@
+def split_and_join(line):
+    splitnjoin = line.split()
+    return "-".join(splitnjoin)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
