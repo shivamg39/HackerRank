e03f4cf379711c0f3448936a366f330cf80be3d5
Shivam Gupta <shivam39@outlook.com>
2019-06-07 12:30:01 -0700

    Update solution.py

--- Python/Strings/What's Your Name/solution.py
+++ Python/Strings/What's Your Name/solution.py
@@ -1,4 +1,5 @@
 def print_full_name(a, b):
+    #print("Hello %s %s! You just delved into python." % (a,b))
     print("Hello ", a, " ", b, "! You just delved into python.", sep="")
 
 if __name__ == '__main__':
