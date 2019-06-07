878be5cd743bc5e0e547cc3852b04815f1b724cb
shivamg39 <Shivam.Gupta-SA@csulb.edu>
2019-06-07 08:39:43 -0700

    changes

---
+++ Python/Basic Data Types/Lists/solution.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    N = int(input())
+    Nlist = []
+    for times in range(N):
+        command, *line = input().split()
+        if (command == "insert"):
+            Nlist.insert(int(line[0]), int(line[1]))   
+        elif (command == "print"):
+            print(Nlist)
+        elif (command == "remove"):
+            Nlist.remove(int(line[0]))
+        elif (command == "append"):
+            Nlist.append(int(line[0]))
+        elif (command == "sort"):
+            Nlist.sort()
+        elif (command == "pop"):
+            Nlist.pop()
+        elif (command == "reverse"):
+            Nlist.reverse()
+        else:
+            continue
