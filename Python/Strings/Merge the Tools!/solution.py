275d1c4cb9906aef56692aa8f0614b031f97f4f4
Shivam Gupta <shivam39@outlook.com>
2019-06-09 13:41:37 -0700

    Update solution.py

---
+++ Python/Strings/Merge the Tools!/solution.py
@@ -0,0 +1,16 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+
+        temp = string[i:i+k]
+
+        word = ""
+
+        for char in temp:
+            if char not in word:
+                word += char
+
+        print(word)
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
