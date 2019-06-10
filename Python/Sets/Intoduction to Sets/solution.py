9df085f0dd44c2383b9b36ad9243ccd2bf764db6
Shivam Gupta <shivam39@outlook.com>
2019-06-10 12:58:39 -0700

    Create solution.py

---
+++ Python/Sets/Intoduction to Sets/solution.py
@@ -0,0 +1,12 @@
+def average(array):
+    arr = set(array)
+    avg = 0
+    for num in arr:
+        avg += num
+    return avg / len(arr)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
