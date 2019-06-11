db83f92715bdbee8ca44258a72ad71beef69e7d5
Shivam Gupta <shivam39@outlook.com>
2019-06-11 10:55:55 -0700

    Create solution.py

---
+++ Python/Sets/Set .discard(), .remove() & .pop()/solution.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = set(map(int, input().split()))
+
+for times in range(int(input())):
+    commands = input().split()
+    if (commands[0] == "pop"):
+        s.pop()
+    elif (commands[0] == "remove"):
+        s.remove(int(commands[1]))
+    elif (commands[0] == "discard"):
+        s.discard(int(commands[1]))
+
+total = 0
+for num in s:
+    total += num
+print(total)
