29d136630f3a09b52ac57bbf8a41f1fac3205db5
Shivam Gupta <shivam39@outlook.com>
2019-06-11 11:09:05 -0700

    Create solution.py

---
+++ Python/Sets/Check Subset/solution.py
@@ -0,0 +1,10 @@
+for times in range(int(input())):
+    input()
+    A = set(map(int, input().split()))
+    input()
+    B = set(map(int, input().split()))
+    subSet = True
+    for num in A:
+        if num not in B:
+            subSet = False
+    print(subSet)
