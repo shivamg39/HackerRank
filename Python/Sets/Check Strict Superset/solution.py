35180f206c99d8c1124915609b1d7dbe0e6ff97f
Shivam Gupta <shivam39@outlook.com>
2019-06-11 16:18:24 -0700

    Update solution.py

--- Python/Sets/Check Strict Superset/solution.py
+++ Python/Sets/Check Strict Superset/solution.py
@@ -1,1 +1,10 @@
-
+arr = set(map(int, input().split()))
+s = 0
+for i in range(int(input())):
+    arr1 = set(map(int, input().split()))
+    if arr.issuperset(arr1) != True:
+        s = 1
+if s == 1:
+    print(False)
+else:
+    print(True)
