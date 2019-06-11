2cb3b22aedbb1c941e3012ba0c2ee33d5d8c61bd
Shivam Gupta <shivam39@outlook.com>
2019-06-11 09:47:10 -0700

    Create solution.py

---
+++ Python/Sets/Set Mutations/solution.py
@@ -0,0 +1,21 @@
+input()
+A = set(map(int, input().split(" ")))
+
+for times in range(int(input())):
+    command, size = input().split(" ")
+    N = set(map(int, input().split(" ")))
+
+    if (command == "intersection_update"):
+        A.intersection_update(N)
+    elif (command == "update"):
+        A.update(N)
+    elif (command == "difference_update"):
+        A.difference_update(N)
+    elif (command == "symmetric_difference_update"):
+        A.symmetric_difference_update(N)
+
+sum = 0
+for num in A:
+    sum += num
+
+print(sum)
