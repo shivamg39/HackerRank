b7724ae9ce892c11752e83da734c880bbd097511
Shivam Gupta <shivam39@outlook.com>
2019-06-12 08:32:44 -0700

    Create solution.py

---
+++ Python/Sets/The Captain's Room/solution.py
@@ -0,0 +1,14 @@
+input()
+L = list(map(int, input().split()))
+s1=set();
+s2=set();
+for num in L:
+    if num in s1:
+        s2.add(num)
+    else:
+        s1.add(num)
+
+s3 = s1.difference(s2)
+
+for num in s3:
+    print(num)
