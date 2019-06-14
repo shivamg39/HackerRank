cdbee364dec403d21eecb3899b9198acca2f8a57
Shivam Gupta <shivam39@outlook.com>
2019-06-14 08:56:14 -0700

    Update solution.py

--- Java/Introduction/Java If-Else/solution.py
+++ Java/Introduction/Java If-Else/solution.py
@@ -1,1 +1,23 @@
+import java.util.*;
 
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.close();
+
+        if (N % 2 == 0) {
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        } else {
+            System.out.println("Weird");
+        }
+    }
+}
