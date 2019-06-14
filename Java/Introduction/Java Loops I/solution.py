eb19577726e4748a4cb51e1438bd0d7852f04613
Shivam Gupta <shivam39@outlook.com>
2019-06-14 09:08:50 -0700

    Update solution.py

--- Java/Introduction/Java Loops I/solution.py
+++ Java/Introduction/Java Loops I/solution.py
@@ -1,1 +1,15 @@
+import java.util.*;
 
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+    }
+}
