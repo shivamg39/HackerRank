2442d1aef4a1cc1f7e51b9fb2fa5d23dcecb52f5
Shivam Gupta <shivam39@outlook.com>
2019-06-14 15:23:11 -0700

    Update solution.py

--- Java/Introduction/Java End-of-file/solution.py
+++ Java/Introduction/Java End-of-file/solution.py
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArrayList<String> as = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()) {
+            String s = scan.nextLine();
+            as.add(s);
+        }
+        for (int i = 1; i <= as.size(); i++) {
+            System.out.println(i +" "+ as.get(i-1));
+        }
+    }
+}
 
