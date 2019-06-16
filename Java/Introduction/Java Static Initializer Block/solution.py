6f68682e06c64e814f458e5bc4fe6c458944ecc1
Shivam Gupta <shivam39@outlook.com>
2019-06-16 16:28:58 -0700

    Update solution.py

--- Java/Introduction/Java Static Initializer Block/solution.py
+++ Java/Introduction/Java Static Initializer Block/solution.py
@@ -15,14 +15,10 @@ static {
         System.out.println("java.lang.Exception: Breadth and height must be positive");
     }
 }
-
 public static void main(String[] args){
 		if(flag){
 			int area=B*H;
 			System.out.print(area);
 		}
-		
-	}//end of main
-
-}//end of class
-
+	}
+}
